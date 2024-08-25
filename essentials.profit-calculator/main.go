c229699e873f84af0c6e971926d3a90b6f5fd5c2
Aleksey Kolesnikov <>
2024-08-25 18:30:51 +0300

    investment calculator

---
+++ essentials.profit-calculator/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue int32
+	var expenses int32
+	var taxRatePercent float64
+
+	fmt.Print("Input Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Input Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Input Tax Rate Percentage: ")
+	fmt.Scan(&taxRatePercent)
+
+	var earningsBeforeTax float64 = float64(revenue - expenses)
+	var earningsAfterTax float64 = earningsBeforeTax * (1 - taxRatePercent/100)
+	var earningsRatio = earningsBeforeTax / earningsAfterTax
+
+	fmt.Println("Earnings Before Tax: ", earningsBeforeTax)
+	fmt.Println("Earnings After Tax: ", earningsAfterTax)
+	fmt.Println("Earnings Ratio: ", earningsRatio)
+}
