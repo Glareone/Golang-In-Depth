376f637727f9b446ef0ff631ab515dedea579cc5
Aleksey Kolesnikov <>
2024-09-15 22:30:34 +0300

    dereferencing

--- structs-and-custom-types/structs.go
+++ structs-and-custom-types/structs.go
@@ -18,13 +18,38 @@ func main() {
 		createdAt: time.Now(),
 	}
 
-	// ... do something awesome with that gathered data!
+	// Shorthand initialization
+	var appUser2ShortHandInit = User{
+		userFirstName,
+		userLastName,
+		userBirthdate,
+		time.Now(),
+	}
 
-	fmt.Println(appUser.firstName, appUser.lastName, appUser.birthDate)
+	var appUser3EmptyStruct = User{}
+
+	// ... do something awesome with that gathered data!
+	fmt.Println("appUser: ")
+	outputUserDetails(appUser)
+	fmt.Println("appUser2ShortHandInit: ")
+	outputUserDetailsPointer(&appUser2ShortHandInit)
+	fmt.Println("appUser2ShortHandInit: ", appUser2ShortHandInit.firstName, appUser2ShortHandInit.lastName, appUser2ShortHandInit.birthDate)
+	fmt.Println("appUser3EmptyStruct, User Struct to string:", appUser3EmptyStruct)
 }
 
-func outputUserDetails(firstName string, lastName string, birthDate string) {
+// "user" is a copy of the struct we sent to the function calling it with "outputUserDetails(appUser)"
+// in order to use a pointer we need to change the declaration
+func outputUserDetails(user User) {
+	fmt.Println("appUser: ", user.firstName, user.lastName, user.birthDate, user.createdAt)
+}
 
+func outputUserDetailsPointer(user *User) {
+	// technically, calling user.firstName without using asterisk there like *user.firstName
+	// the technically correct way to use it would be using dereference: (*user).firstName
+	// but Go allows us to use the shorthand
+	fmt.Println("appUser: ", user.firstName, user.lastName, user.birthDate, user.createdAt)
+	// returns the same, normally you can dereference the value, but Go allows us to use the shorthand from above
+	fmt.Println("appUser using reference: ", (*user).firstName, (*user).lastName, (*user).birthDate, (*user).createdAt)
 }
 
 func getUserData(promptText string) string {
